d0ce8c2eb483579046bd4371074b8193d9e18db5
Ero Silva <eronildo.silva@outlook.com>
2018-08-15 19:23:27 -0300

    Melhorias na visualização do código.

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,19 +1,15 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpackConfig = require('./webpack.config');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(webpackConfig, {
-
     devtool: false,
-
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
     },
-
     plugins: [
         new CleanWebpackPlugin(['dist'])
     ]
-
 });
