58539e846dfdd661abf567e191410b90486683e6
Erô Silva <eronildo.silva@outlook.com>
2020-07-20 14:19:22 -0300

    Adiciona deploy pelo CLI gh-pages

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpackConfig = require('./webpack.config')
 
 module.exports = merge(webpackConfig, {
@@ -10,7 +10,7 @@ module.exports = merge(webpackConfig, {
     path: path.join(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
   },
-  plugins: [new CleanWebpackPlugin(['dist'])],
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
       // SASS
